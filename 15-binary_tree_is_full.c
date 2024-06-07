22338ba35a96f82459c704ea75488af96b4a2827
stringpappy <jejeodunayo70@gmail.com>
2024-06-07 02:56:59 -0700

    binary tree

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -2,7 +2,7 @@
 /**
  * binary_tree_is_full -  function that checks if a binary tree is full
  * @tree: a pointer to the root node of the tree to check
- * Retub : 0
+ * Return: 0
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
