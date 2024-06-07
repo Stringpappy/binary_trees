f2a7a9437b20a05000a9b6743d3a0ed3fda7b9f2
stringpappy <jejeodunayo70@gmail.com>
2024-06-07 14:27:29 -0700

    binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,18 +6,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t lt_height, rt_height;
+	size_t height_l = 0;
+	size_t height_r = 0;
 
-
-
-	if (tree == NULL)
-	{
+	if (!tree)
 		return (0);
-	}
-	lt_height = binary_tree_height(tree->left);
-	rt_height = binary_tree_height(tree->right);
-	
-	return 1 + (lt_height > rt_height ? lt_height : rt_height);
-	 if (lt_height != rt_height)
-		 return (0);
+
+	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (height_l > height_r ? height_l : height_r);
 }
